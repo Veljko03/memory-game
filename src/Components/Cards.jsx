150afdc772dd43a32eed64a327a5cf95c8d1a284
Veljko03 <veljkodovragovic@gmail.com>
2024-09-23 15:50:46 +0200

    implemented logic for updating curr and best score

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -33,25 +33,40 @@ function Cards({ one, two, three, four, five, six, seven, eight, nine, ten }) {
     } else {
       setCheckArr([...chekArr, cardID]);
       setCurrentScore((c) => c + 1);
+      console.log(currentScore + "in if");
     }
 
     setAllCards(shuffleArray(allCards));
   };
+  useEffect(() => {
+    if (currentScore >= bestScore) {
+      setBestScore(currentScore);
+    }
+  }, [currentScore]);
 
-  console.log(currentScore);
+  console.log(currentScore + "curr");
+  console.log(bestScore + "best");
 
-  return (
-    <div>
-      {allCards.map((c) => (
-        <Card
-          key={c.id}
-          value={c.id}
-          character={c}
-          handleClick={handleCardClick}
-        />
-      ))}
-    </div>
-  );
+  if (bestScore == 10) {
+    return (
+      <div>
+        <h1>You won man!</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {allCards.map((c) => (
+          <Card
+            key={c.id}
+            value={c.id}
+            character={c}
+            handleClick={handleCardClick}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Cards;
